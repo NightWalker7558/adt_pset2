ea65854c6a1d635b06424b4d992b72e9c7d20e7b
itsAdee <adeelahmedqureshi02@gmail.com>
2023-11-17 11:48:22 +0500

    Implementation of ConcreteEdgesGraphTest

--- test/graph/ConcreteEdgesGraphTest.java
+++ test/graph/ConcreteEdgesGraphTest.java
@@ -16,30 +16,63 @@ import org.junit.Test;
  * Tests against the Graph spec should be in GraphInstanceTest.
  */
 public class ConcreteEdgesGraphTest extends GraphInstanceTest {
-    
+
     /*
      * Provide a ConcreteEdgesGraph for tests in GraphInstanceTest.
      */
-    @Override public Graph<String> emptyInstance() {
+    @Override
+    public Graph<String> emptyInstance() {
         return new ConcreteEdgesGraph();
     }
-    
+
     /*
      * Testing ConcreteEdgesGraph...
      */
-    
+
     // Testing strategy for ConcreteEdgesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteEdgesGraph.toString()
-    
+    // - Test when the graph is empty
+    // - Test when the graph has vertices and edges
+
+    @Test
+    public void testConcreteEdgesGraphToString() {
+        Graph<String> graph = emptyInstance();
+        assertEquals("", graph.toString()); // Empty graph
+
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 3);
+        graph.set("B", "A", 2);
+
+        assertEquals("(A -> B, weight: 3)\n(B -> A, weight: 2)\n", graph.toString());
+    }
+
     /*
      * Testing Edge...
      */
-    
+
     // Testing strategy for Edge
-    //   TODO
-    
-    // TODO tests for operations of Edge
-    
+    // - Test the constructor
+    // - Test getSource()
+    // - Test getTarget()
+    // - Test getWeight()
+    // - Test setWeight()
+
+    @Test
+    public void testEdge() {
+        Edge edge = new Edge("A", "B", 3);
+        assertEquals("A", edge.getSource());
+        assertEquals("B", edge.getTarget());
+        assertEquals(3, edge.getWeight());
+
+        edge.setWeight(5);
+        assertEquals(5, edge.getWeight());
+    }
+
+    // Add more tests for various scenarios and edge cases
+
+    @Test
+    public void testEdgeToString() {
+        Edge edge = new Edge("A", "B", 3);
+        assertEquals("(A -> B, weight: 3)", edge.toString());
+    }
 }
