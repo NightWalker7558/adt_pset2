dfdfac5f9d0d31be6f9e21a0e82518bbd4707028
Abdul Arham <abdul.arham9123@gmail.com>
2023-11-17 11:44:18 +0500

    added test cases in GraphInstaneTest

--- test/graph/GraphInstanceTest.java
+++ test/graph/GraphInstanceTest.java
@@ -1,46 +1,88 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package graph;
-
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
-/**
- * Tests for instance methods of Graph.
- * 
- * <p>PS2 instructions: you MUST NOT add constructors, fields, or non-@Test
- * methods to this class, or change the spec of {@link #emptyInstance()}.
- * Your tests MUST only obtain Graph instances by calling emptyInstance().
- * Your tests MUST NOT refer to specific concrete implementations.
- */
 public abstract class GraphInstanceTest {
-    
-    // Testing strategy
-    //   TODO
-    
+
+    // Existing tests...
+
+    @Test
+    public void testAddVertex() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("A"));
+        assertTrue(graph.vertices().contains("A"));
+    }
+
+    @Test
+    public void testRemoveVertex() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        assertTrue(graph.remove("A"));
+        assertFalse(graph.vertices().contains("A"));
+    }
+
+    @Test
+    public void testSetEdge() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+
+        // Add edge with weight
+        assertEquals(0, graph.set("A", "B", 3));
+        assertTrue(graph.targets("A").containsKey("B"));
+        assertEquals(3, (int) graph.targets("A").get("B"));
+
+        // Update edge weight
+        assertEquals(3, graph.set("A", "B", 5));
+        assertEquals(5, (int) graph.targets("A").get("B"));
+
+        // Remove edge
+        assertEquals(5, graph.set("A", "B", 0));
+        assertFalse(graph.targets("A").containsKey("B"));
+    }
+
+    @Test
+    public void testRemoveEdge() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+
+        graph.set("A", "B", 3);
+        assertTrue(graph.targets("A").containsKey("B"));
+
+        assertTrue(graph.set("A", "B", 0) > 0);
+        assertFalse(graph.targets("A").containsKey("B"));
+    }
+
+    @Test
+    public void testSourcesAndTargets() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.add("C");
+
+        graph.set("A", "B", 2);
+        graph.set("A", "C", 3);
+
+        Map<String, Integer> sources = graph.sources("B");
+        assertEquals(2, (int) sources.get("A"));
+
+        Map<String, Integer> targets = graph.targets("A");
+        assertEquals(2, (int) targets.get("B"));
+        assertEquals(3, (int) targets.get("C"));
+    }
+
+    // Additional tests for other methods...
+
     /**
      * Overridden by implementation-specific test classes.
      * 
      * @return a new empty graph of the particular implementation being tested
      */
     public abstract Graph<String> emptyInstance();
-    
-    @Test(expected=AssertionError.class)
-    public void testAssertionsEnabled() {
-        assert false; // make sure assertions are enabled with VM argument: -ea
-    }
-    
-    @Test
-    public void testInitialVerticesEmpty() {
-        // TODO you may use, change, or remove this test
-        assertEquals("expected new graph to have no vertices",
-                Collections.emptySet(), emptyInstance().vertices());
-    }
-    
-    // TODO other tests for instance methods of Graph
-    
 }
